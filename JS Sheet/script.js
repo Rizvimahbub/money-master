17f1da0daf6793d6cdfc8d32f7849835cd11252e
Rizvimahbub <rizvialam007@gmail.com>
2022-02-17 21:47:37 +0600

    second commit

--- JS Sheet/script.js
+++ JS Sheet/script.js
@@ -5,7 +5,7 @@ let clothesCost = document.getElementById('clothes-cost');
 let totalExpenses = document.getElementById('total-expenses');
 let restBalance = document.getElementById('rest-balance');   
 let savingPercentageInput = document.getElementById('saving-percentage');
-let totalSavingAmout = document.getElementById('saving-amount');
+let totalSavings = document.getElementById('saving-amount');
 let balanceRemaining = document.getElementById('remaining-balance');
 
 function totalExpensesCount(){
@@ -27,7 +27,18 @@ function balance(){
 
     let balanceRemaining = salary - totalExpensesCount();
 
-    restBalance.innerText = balanceRemaining;
+    if(document.getElementById('total-income').value.trim() == ''){
+
+        document.getElementById('rest-balance').innerText = 'Invalid amount';
+        
+    }
+    else{
+
+        restBalance.innerText = balanceRemaining;
+
+    }
+
+    
 
     return balanceRemaining;
 
@@ -37,20 +48,41 @@ function savingsAmount(){
 
     let salary = parseFloat(salaryAmount.value);
 
+    let restBalanceAmount = parseFloat(restBalance.innerText);
+
     let savingPercentage = parseFloat(savingPercentageInput.value) / 100;
 
     let savingTotal = savingPercentage * salary;
 
-    totalSavingAmout.innerText = savingTotal;
+    if(restBalanceAmount < savingTotal){
 
+        totalSavings.innerText = 'Invalid amount'
+
+    }
+    else{
+
+        totalSavings.innerText = savingTotal;
+
+    }
     return savingTotal;
 }
+
 
 function remainingBalance(){
 
     let remainingBalanceAmount = balance() - savingsAmount();
 
-    balanceRemaining.innerText = remainingBalanceAmount;
+    if(remainingBalanceAmount < 0){
+
+        balanceRemaining.innerText = 'Invalid amount';
+
+    }
+    else{
+
+        balanceRemaining.innerText = remainingBalanceAmount;
+
+    }
+    
 
     return remainingBalanceAmount;
 
@@ -70,45 +102,256 @@ function negativeErrorHandling(){
 
     let inputClothesCost = parseFloat(clothesCost.value);
     document.getElementById('clothes-cost').value = inputClothesCost;
+    
+    let totalExpensesAmount = parseFloat(totalExpenses.innerText);
+    document.getElementById('total-expenses').innerText = totalExpensesAmount;
+
 
     if(salary < 0 && inputFoodCost < 0 && inputRentCost < 0 && inputClothesCost < 0){
+
         document.getElementById('income-error-msg').innerText = 'Please type a positive number';
         document.getElementById('food-error-msg').innerText = 'Please type a positive number';
         document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
         document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
-        
-        document.getElementById('total-income').style.border = '1px solid red';
-        document.getElementById('food-income').style.border = '1px solid red';
-        document.getElementById('rent-cost').style.border = '1px solid red';
-        document.getElementById('clothes-cost').style.border = '1px solid red';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
     }
-    else if(salary < 0){
+    else if(salary < 0 && inputFoodCost < 0 && inputRentCost < 0){
+
         document.getElementById('income-error-msg').innerText = 'Please type a positive number';
-        document.getElementById('total-income').style.border = '1px solid red';
+        document.getElementById('food-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(salary < 0 && inputClothesCost < 0 && inputRentCost < 0){
+
+        document.getElementById('income-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(salary < 0 && inputClothesCost < 0 && inputFoodCost < 0){
+
+        document.getElementById('income-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('food-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(inputRentCost < 0 && inputClothesCost < 0 && inputFoodCost < 0){
+
+        document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('food-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(salary < 0 && inputFoodCost < 0){
+
+        document.getElementById('income-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('food-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(salary < 0 && inputRentCost < 0){
+
+        document.getElementById('income-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(salary < 0 && inputClothesCost < 0){
+
+        document.getElementById('income-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(inputFoodCost < 0 && inputClothesCost < 0){
+
+        document.getElementById('food-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(inputFoodCost < 0 && inputRentCost < 0){
+
+        document.getElementById('food-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(inputClothesCost < 0 && inputRentCost < 0){
+
+        document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
     }
     else if(inputFoodCost < 0){
+
         document.getElementById('food-error-msg').innerText = 'Please type a positive number';
-        document.getElementById('food-cost').style.border = '1px solid red';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
     }
     else if(inputRentCost < 0){
+
         document.getElementById('rent-error-msg').innerText = 'Please type a positive number';
-        document.getElementById('rent-cost').style.border = '1px solid red';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
     }
     else if(inputClothesCost < 0){
+        
         document.getElementById('clothes-error-msg').innerText = 'Please type a positive number';
-        document.getElementById('clothes-cost').style.border = '1px solid red';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(salary < 0){
+
+        document.getElementById('income-error-msg').innerText = 'Please type a positive number';
+        document.getElementById('total-expenses').innerText = '';
+        document.getElementById('rest-balance').innerText = '';
+        document.getElementById('saving-amount').innerText = '';
+        document.getElementById('remaining-balance').innerText = '';
+
+    }
+    else if(totalExpensesAmount > salary){
+
+        document.getElementById('total-expenses').innerText = 'Your can not spend more money than your income';
+
+        document.getElementById('total-expenses').style.color = 'red';
+
+        document.getElementById('rest-balance').innerText = 'Invalid amount';
+
+        document.getElementById('rest-balance').style.color = 'red';
+
+        document.getElementById('saving-amount').innerText = '';
+
+        document.getElementById('remaining-balance').innerText = '';
+
+        document.getElementById('income-error-msg').innerText = '';
+
+        document.getElementById('food-error-msg').innerText = '';
+
+        document.getElementById('rent-error-msg').innerText = '';
+
+        document.getElementById('clothes-error-msg').innerText = '';
+
+    }
+    else{
+
+        document.getElementById('income-error-msg').innerText = '';
+
+        document.getElementById('food-error-msg').innerText = '';
+
+        document.getElementById('rent-error-msg').innerText = '';
+
+        document.getElementById('clothes-error-msg').innerText = '';
+        
+         
+    }
+
+    return;
+}
+
+
+function emptyErrorHandling(){
+
+    let salary = parseFloat(salaryAmount.value);
+    document.getElementById('total-income').value = salary;
+    
+    let inputFoodCost = parseFloat(foodCost.value);
+    document.getElementById('food-cost').value = inputFoodCost;
+
+    let inputRentCost = parseFloat(rentCost.value);
+    document.getElementById('rent-cost').value = inputRentCost;
+
+    let inputClothesCost = parseFloat(clothesCost.value);
+    document.getElementById('clothes-cost').value = inputClothesCost;
+    
+    let totalExpensesAmount = parseFloat(totalExpenses.innerText);
+    document.getElementById('total-expenses').innerText = totalExpensesAmount;
+
+    if(document.getElementById('total-income').value.trim() == ''){
+
+        alert( "Please put an amount in the income amount box" );
+        
+    }
+    else if(document.getElementById('food-cost').value.trim() == ''){
+
+        alert( "Please put an amount in the food amount box" );
+        
+    }
+    else if(document.getElementById('rent-cost').value.trim() == ''){
+
+        alert( "Please put an amount in the rent amount box" );
+        
+    }
+    else if(document.getElementById('clothes-cost').value.trim() == ''){
+
+        alert( "Please put an amount in the  amount box" );
+        
     }
     return;
 }
 
 
+
 document.getElementById('expense-calculate-btn').addEventListener('click',function(){
+
+    
 
     totalExpensesCount();
 
     balance();
 
     negativeErrorHandling();
+
+    emptyErrorHandling();
+
 });
 
 
@@ -117,7 +360,9 @@ document.getElementById('percentage-calculate-btn').addEventListener('click',fun
 
     savingsAmount();
 
-    remainingBalance()
+    remainingBalance();
+
+    negativeErrorHandling();
 
 });
 
